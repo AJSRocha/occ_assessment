a424dddda4de6b1755be822a5674244686afae94
Arocha <alberto.jmas.rocha@gmail.com>
2024-04-05 01:38:44 +0100

    yap

--- spline.R
+++ spline.R
@@ -3,10 +3,13 @@ library(ggplot2)
 library(dplyr)
 library(Runuran)
 
-load("data/nautilus_occ.Rdata")
+# load("data/nautilus_occ.Rdata")
 
 lota_naut_2$month = case_when(lota_naut_2$MES %in% c('10', '11', '12') ~ lota_naut_2$MES,
                               T ~ paste0("0",lota_naut_2$MES))
+
+lota_naut_2 = lota_naut_2 %>% 
+  mutate(week = lubridate::week(DATA))
 
 lota_naut_2$pesos = 1/
 ((lota_naut_2$peso_am_caixa / lota_naut_2$peso_total_caixa) *
@@ -18,15 +21,15 @@ hist(lota_naut_2$pesos)
 
 ?loess
 
-ajuste = loess(peso_total ~ month,
+ajuste = loess(peso_total ~ week,
                data = lota_naut_2,
                degree = 2)
 
 lota_naut_2 %>% 
   # group_by(MES) %>% 
   ggplot +
-  geom_point(aes(x = month, y = peso_total)) +
-  geom_line(aes(x = month, y = fitted(ajuste), group = 1), col = 'purple') +
-  geom_point(aes(x = month, y = fitted(ajuste)), col = 'red') +
+  geom_point(aes(x = week, y = peso_total)) +
+  geom_line(aes(x = week, y = fitted(ajuste), group = 1), col = 'purple') +
+  geom_point(aes(x = week, y = fitted(ajuste)), col = 'red') +
   theme_bw()
 
